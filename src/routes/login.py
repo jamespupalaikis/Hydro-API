b9940ef67299987263e94d382bcbc147d4893e4a
James Pupalaikis <jamesppup@gmail.com>
2025-12-23 23:30:09 -0500

    frontend files
    add index files for frontend.

--- src/routes/login.py
+++ src/routes/login.py
@@ -12,10 +12,12 @@ router = APIRouter()
 
 
 
-@router.get("/login", tags=["login"])
+@router.post("/login", tags=["login"])
 def set_login_cookie(username, password, response:Response):
 
     response.set_cookie(key="creds", value=f"buffer{username}:::{password}")
+    
+    return "ok"
     pass
 
 #@router.get("/get-creds", tags=["login"])
@@ -32,10 +34,12 @@ def verify_credentials(request:Request):
     given =request.cookies.get("creds")
     print(given,"--  --  --", creds_GT)
     if not (given == creds_GT):
+        
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Basic"},
         )
+        return False
     return True
     
